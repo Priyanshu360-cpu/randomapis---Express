28b92e05834799dc3a6ac12dab4a732121161d24
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-05 17:45:56 +0530

    managed cches

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,9 @@ var time ={
   date:date,
   time:time,
 }
-var movies = [
-  {id: 101, name: "Fight Club", year: 1999, rating: 8.1},
-  {id: 102, name: "Inception", year: 2010, rating: 8.7},
-  {id: 103, name: "The Dark Knight", year: 2008, rating: 9},
-  {id: 104, name: "12 Angry Men", year: 1957, rating: 8.9}
-];
+
 router.get('/', function(req, res, next) {
-  res.json(movies);
+  res.render('index', { title: 'RandomApi' });
 });
 router.get('/time', function(req, res, next) {
   if([...pass["Passwords"]].indexOf(req.url.split("=")[1])!=-1){
