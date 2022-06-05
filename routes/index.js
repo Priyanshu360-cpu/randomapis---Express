f92c227f150ceb67927c9c208dbb5039f005ac6e
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-05 17:10:59 +0530

    added eval res

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { evaluate } = require('mathjs')
 var pass = require('./passwords.json');
-var image = require('./image.jpg')
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var time ={
   date:date,
   time:time,
-  image:image
 }
 var movies = [
   {id: 101, name: "Fight Club", year: 1999, rating: 8.1},
@@ -27,5 +26,19 @@ router.get('/time', function(req, res, next) {
   }else{
     res.send("Wrong key")
   }
+  
 });
+router.get('/evaluate', function(req, res, next) {
+  let arr=[...pass["Passwords"]]
+  if(arr.indexOf(req.url.split("=")[1].split("?")[0])!=-1){
+    let a =req.url.split("=")[2];
+    let result={
+      result:evaluate(a)
+    }
+  res.json(result);
+
+  }else{
+    res.send("Wrong key")
+  }
+})
 module.exports = router;
