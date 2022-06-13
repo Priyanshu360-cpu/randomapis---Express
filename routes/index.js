6e82404c2639fd169ac8b85501efed41a2d97e80
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-06-13 19:20:19 +0530

    changed method for securing api

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const { Console } = require('console');
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 const { evaluate } = require('mathjs')
+const path = require('path');
 var pass = require('./passwords.json');
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -15,35 +17,48 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'RandomApi' });
 });
 router.get('/time', function(req, res, next) {
-  if([...pass["Passwords"]].indexOf(req.url.split("=")[1])!=-1){
+  if(pass["Passwords"][req.url.split("=")[1].split("?")[0]]){
+    if((pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]<=100)
+    ||(pass["Passwords"][req.url.split("=")[1].split("?")[0]]["permisson"]=="admin")){
+    pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]=pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]+1;
+    pass["Passwords"][req.url.split("=")[1].split("?")[0]]["lastsearch"]=[...(pass["Passwords"][req.url.split("=")[1].split("?")[0]]["lastsearch"]),"time"]
+    fs.writeFileSync(path.join(__dirname,'./passwords.json'),JSON.stringify(pass,null, 2));
   res.json(time);
-
+    }else{
+      res.send("LIMIT EXCEEDED")
+    }
   }else{
     res.send("Wrong key")
   }
   
 });
 router.get('/evaluate', function(req, res, next) {
-  if([...pass["Passwords"]].indexOf(req.url.split("=")[1].split("?")[0])!=-1){
+  if(pass["Passwords"][req.url.split("=")[1].split("?")[0]]){
+    if((pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]<=100)
+    ||(pass["Passwords"][req.url.split("=")[1].split("?")[0]]["permisson"]=="admin")){
+    pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]=pass["Passwords"][req.url.split("=")[1].split("?")[0]]["usage"]+1;
+    pass["Passwords"][req.url.split("=")[1].split("?")[0]]["lastsearch"]=[...(pass["Passwords"][req.url.split("=")[1].split("?")[0]]["lastsearch"]),"evaluate"]
+    fs.writeFileSync(path.join(__dirname,'./passwords.json'),JSON.stringify(pass,null, 2));
     let a =req.url.split("=")[2];
     let result={
       result:evaluate(a)
     }
   res.json(result);
-
   }else{
+res.send("Limit Exceeded")
+  }}else{
     res.send("Wrong key")
   }
 })//http://localhost:3000/evaluate?key=123456?data=2+9+829292
 router.get('/password',function(req,res,next){
-  const path = require('path');
- let a=req.url.split("=")[1];
- if([...pass["Passwords"]].indexOf(a)==-1){
- let c={
-   Passwords:[...pass["Passwords"],a]
- }
- fs.writeFileSync(path.join(__dirname,'./passwords.json'),JSON.stringify(c,null, 2));
- res.json(c);
+ if(!pass["Passwords"][req.url.split("=")[1]]){
+  pass["Passwords"][req.url.split("=")[1]]={
+    "usage":0,
+    "permission":"user",
+    "lastsearch":[]
+  }
+ fs.writeFileSync(path.join(__dirname,'./passwords.json'),JSON.stringify(pass,null, 2));
+ res.json(pass);
  pass=require('./passwords.json');
 }else{
   res.send("Password already exists")
